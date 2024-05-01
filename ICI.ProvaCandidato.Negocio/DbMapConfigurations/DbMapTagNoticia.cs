ad9e966a8fe371d8ad81354a7e75236c6d4a4fce
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-01 16:26:15 -0300

    configurando contexto e mapeamento de tabelas

---
+++ ICI.ProvaCandidato.Negocio/DbMapConfigurations/DbMapTagNoticia.cs
@@ -0,0 +1,17 @@
+﻿using ICI.ProvaCandidato.Dados.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ICI.ProvaCandidato.Negocio.DbMapConfigurations
+{
+    public class DbMapTagNoticia : IEntityTypeConfiguration<TagNoticia>
+    {
+        public void Configure(EntityTypeBuilder<TagNoticia> builder)
+        {
+            builder.ToTable("TagNoticias");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.NoticiaId).HasColumnType("INTERGER").IsRequired();
+            builder.Property(x => x.TagId).HasColumnType("INTERGER").IsRequired();
+        }
+    }
+}
