8f45d680832409fc8ae9e59c271ffd0177842f7b
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-01 23:10:43 -0300

    controlador de noticias

---
+++ ICI.ProvaCandidato.Negocio/Controllers/NoticiaController.cs
@@ -0,0 +1,25 @@
+﻿using ICI.ProvaCandidato.Negocio.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Negocio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly INoticiaRepository repository;
+
+        public NoticiaController(INoticiaRepository context)
+        {
+            repository = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var news = await repository.GetAll();
+            return Ok(news);
+        }
+    }
+}
