ad4dc88fa6af90aab17dd7c6b76bd37ec8d4f6eb
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-02 02:05:01 -0300

    Implementado repository para regras de negocio para tags

---
+++ ICI.ProvaCandidato.Dados/Interface/ITagRuleRepository.cs
@@ -0,0 +1,10 @@
+﻿using ICI.ProvaCandidato.Dados.Dto;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Dados.Interface
+{
+    public interface ITagRuleRepository
+    {
+        public Task<bool> AlreadyExist(string dtoDescricao);
+    }
+}
---
+++ ICI.ProvaCandidato.Negocio/Repositories/TagRuleRepository.cs
@@ -0,0 +1,24 @@
+﻿using ICI.ProvaCandidato.Dados.Dto;
+using ICI.ProvaCandidato.Dados.Interface;
+using ICI.ProvaCandidato.Negocio.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Negocio.Repositories
+{
+    public class TagRuleRepository : ITagRuleRepository
+    {
+        public readonly SqliteContext _context;
+
+        public TagRuleRepository(SqliteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AlreadyExist(string dtoDescricao)
+        {
+            return await _context.Tags.AnyAsync(x => x.Descricao == dtoDescricao);
+        }
+    }
+}
