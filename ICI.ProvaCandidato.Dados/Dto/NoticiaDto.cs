0e4ea542eb8eb782fe04c637c23438547dd9ffe8
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-01 23:09:05 -0300

    DTO para noticias

---
+++ ICI.ProvaCandidato.Dados/Dto/NoticiaDto.cs
@@ -0,0 +1,11 @@
+﻿using ICI.ProvaCandidato.Dados.Models;
+
+namespace ICI.ProvaCandidato.Dados.Dto
+{
+    public class NoticiaDto
+    {
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
