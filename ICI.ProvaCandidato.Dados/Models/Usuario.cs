d86a8c14b1f29fa6b481793a45ee6e1ee2b1dc3f
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-01 23:54:53 -0300

    criar convert para dto no usuario

--- ICI.ProvaCandidato.Dados/Models/Usuario.cs
+++ ICI.ProvaCandidato.Dados/Models/Usuario.cs
@@ -1,9 +1,20 @@
-﻿namespace ICI.ProvaCandidato.Dados.Models
+﻿using ICI.ProvaCandidato.Dados.Dto;
+
+namespace ICI.ProvaCandidato.Dados.Models
 {
     public class Usuario : Entity
     {
         public string Nome { get; set; }
         public string Senha { get; set; }
         public string Email {  get; set; }
+
+        public UsuarioDto ConverterToDto()
+        {
+            return new UsuarioDto()
+            {
+                Nome = Nome,
+                Email = Email
+            };
+        }
     }
 }
