2f4474e968c2e1739c24cfdb4dbf13230b8ab108
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-01 23:56:57 -0300

    criando controler para usuario

---
+++ ICI.ProvaCandidato.Negocio/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using ICI.ProvaCandidato.Negocio.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Negocio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        public readonly IUsuarioRepository _repository;
+
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var usuarios = await _repository.GetAll();
+            return Ok(usuarios);
+        }
+    }
+}
