4604aefc0eb189fdd9975e54a2b40d2ec4a89312
leonardoGasperin <leonardovdg@hotmail.com>
2024-05-02 00:21:51 -0300

    implementando controller para tags

---
+++ ICI.ProvaCandidato.Negocio/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using ICI.ProvaCandidato.Negocio.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ICI.ProvaCandidato.Negocio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagController : ControllerBase
+    {
+        public readonly ITagRepository _repotory;
+
+        public TagController(ITagRepository repotory)
+        {
+            _repotory = repotory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tags = await _repotory.GetAll();
+
+            return Ok(tags);
+        }
+    }
+}
